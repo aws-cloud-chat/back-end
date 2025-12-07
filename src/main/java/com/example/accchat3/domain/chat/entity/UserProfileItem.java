427a5b870d217ee103048b915537ba315d3ae498
HONG <hkt03130@gmail.com>
2025-12-07 19:13:26 +0900

    UserprofileItem 엔티티 추가

--- src/main/java/com/example/accchat3/domain/chat/entity/UserProfileItem.java
+++ src/main/java/com/example/accchat3/domain/chat/entity/UserProfileItem.java
@@ -1,7 +1,39 @@
 package com.example.accchat3.domain.chat.entity;
 
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@DynamoDbBean
 public class UserProfileItem {
+
+    private String pk; //User#1001
+    private String sk; //ROOM#R1
+
+    private Long userId;
+    private String username;
+    private String email;
+    private String passwordHash;
+
+    private String lastMessageId;           // MSG#3
+    private String lastMessageContent;      // "ㅇㅇ 잘듣"
+    private String lastMessageSenderUsername;
+    private Long messageUpdatedAt;          // epochMillis 등
+    private String receiverUsername;
+    private Long receiverUserId;
+
+    @DynamoDbPartitionKey
+    public String getPk(){return pk;}
+
+    @DynamoDbSortKey
+    public String getSk() { return sk; }
 
 }
