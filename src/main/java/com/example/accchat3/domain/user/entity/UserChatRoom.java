03847794df298ae78a7d952fee14e939fe6f6aca
seoyeon0103 <angel6462@naver.com>
2025-12-11 20:04:30 +0900

    Feat: UserChatRoom entity 누락된거 추가

--- src/main/java/com/example/accchat3/domain/user/entity/UserChatRoom.java
+++ src/main/java/com/example/accchat3/domain/user/entity/UserChatRoom.java
@@ -1,4 +1,138 @@
 package com.example.accchat3.domain.user.entity;
 
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+
+import java.time.LocalDateTime;
+
+@DynamoDbBean
 public class UserChatRoom {
+    private Integer userId;
+    private Integer createdAt;
+
+    private Integer chatRoomId;
+    private String chatRoomName;
+
+    private Integer lastMessageId;
+    private String lastMessageContent;
+    private LocalDateTime updatedAt;
+    private Integer seq;
+
+    private Integer senderId;
+    private String senderName;
+
+    private Integer receiverId;
+    private String receiverName;
+
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute("userId")
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    @DynamoDbSortKey
+    @DynamoDbAttribute("createdAt")
+    public Integer getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Integer createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @DynamoDbAttribute("chatRoomId")
+    public Integer getChatRoomId() {
+        return chatRoomId;
+    }
+
+    public void setChatRoomId(Integer chatRoomId) {
+        this.chatRoomId = chatRoomId;
+    }
+
+    @DynamoDbAttribute("chatRoomName")
+    public String getChatRoomName() {
+        return chatRoomName;
+    }
+
+    public void setChatRoomName(String chatRoomName) {
+        this.chatRoomName = chatRoomName;
+    }
+
+    @DynamoDbAttribute("lastMessageId")
+    public Integer getLastMessageId() {
+        return lastMessageId;
+    }
+
+    public void setLastMessageId(Integer lastMessageId) {
+        this.lastMessageId = lastMessageId;
+    }
+
+    @DynamoDbAttribute("lastMessageContent")
+    public String getLastMessageContent() {
+        return lastMessageContent;
+    }
+
+    public void setLastMessageContent(String lastMessageContent) {
+        this.lastMessageContent = lastMessageContent;
+    }
+
+    @DynamoDbAttribute("updatedAt")
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @DynamoDbAttribute("seq")
+    public Integer getSeq() {
+        return seq;
+    }
+
+    public void setSeq(Integer seq) {
+        this.seq = seq;
+    }
+
+    @DynamoDbAttribute("senderId")
+    public Integer getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(Integer senderId) {
+        this.senderId = senderId;
+    }
+
+    @DynamoDbAttribute("senderName")
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    @DynamoDbAttribute("receiverId")
+    public Integer getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(Integer receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    @DynamoDbAttribute("receiverName")
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
 }
