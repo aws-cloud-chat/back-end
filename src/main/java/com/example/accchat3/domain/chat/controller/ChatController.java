5034dcbc8e6d66791c33f026fcaa6f2a7e26415e
HONG <hkt03130@gmail.com>
2025-12-11 22:52:08 +0900

    feat: ChatRoom 레포지토리 생성(#7)

--- src/main/java/com/example/accchat3/domain/chat/controller/ChatController.java
+++ src/main/java/com/example/accchat3/domain/chat/controller/ChatController.java
@@ -1,4 +1,14 @@
 package com.example.accchat3.domain.chat.controller;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chats")
 public class ChatController {
+
+
 }
